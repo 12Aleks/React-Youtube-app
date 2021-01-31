4786b70b83d761d5ae42a93be28d13b04189c510
12Aleks <tanais2010@yahoo.com>
2021-01-31 15:29:57 +0100

    added prop-types in project, create test childe components

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,5 +1,25 @@
+import React from 'react'
+import TodoList from "./Todo/TodoList";
+
+const styles = {
+    title: {
+        color: 'red',
+        textTransform: 'uppercase'
+    }
+}
+
 function App() {
-  return <div />
+    const todos = [
+        {id: 1, completed: false, title: 'Test fist'},
+        {id: 2, completed: false, title: 'Test second'},
+        {id: 3, completed: false, title: 'Test third'},
+    ]
+  return (
+      <div className="wrapper">
+            <h1 style={styles.title}>React tutorial</h1>
+            <TodoList  todos={todos}/>
+      </div>
+  )
 }
 
 export default App;
