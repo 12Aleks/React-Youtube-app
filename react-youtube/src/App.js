7f8f5eb07160f5fda2655160812798b25d6a36c3
12Aleks <tanais2010@yahoo.com>
2021-02-01 21:35:33 +0100

    event in react

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -9,17 +9,32 @@ const styles = {
 }
 
 function App() {
-    const todos = [
-        {id: 1, completed: false, title: 'Test fist'},
-        {id: 2, completed: false, title: 'Test second'},
-        {id: 3, completed: false, title: 'Test third'},
-    ]
-  return (
-      <div className="wrapper">
+    const [todos, setTodos] = React.useState(
+        [
+            {id: 1, completed: false, title: 'Test fist'},
+            {id: 2, completed: false, title: 'Test second'},
+            {id: 3, completed: false, title: 'Test third'},
+        ]
+    )
+
+    function onComTodo(id) {
+        console.log(id);
+        setTodos(
+            todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+        )
+    }
+
+    return (
+        <div className="wrapper">
             <h1 style={styles.title}>React tutorial</h1>
-            <TodoList  todos={todos}/>
-      </div>
-  )
+            <TodoList todos={todos} onCom={onComTodo}/>
+        </div>
+    )
 }
 
 export default App;
--- react-youtube/src/Todo/TodoList.js
+++ react-youtube/src/Todo/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from 'prop-types'
 import TodoItem from "./TodoItem";
 
 const styles = {
@@ -10,12 +11,22 @@ const styles = {
     }
 };
 
-export default  function TodoList(props) {
+  function TodoList(props) {
     return(
         <ul style={styles.ul}>
             {props.todos.map((todo, index) => {
-                return  <TodoItem todo={todo} key={todo.id} index={index}/>
+                return  <TodoItem todo={todo} key={todo.id} index={index} onChange={props.onCom}/>
             })}
         </ul>
     )
 }
+
+// props описание типов c помошью prop-types, валидация
+TodoList.propTypes = {
+      //todos - это массив состоящий из объектов с обязательным содержанием
+      todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+      onCom: PropTypes.func.isRequired
+}
+
+
+export default TodoList
